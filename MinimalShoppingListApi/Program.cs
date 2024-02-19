98605c5b84bde0f55a5aae6daf861a461540f96c
IAMOTAS <niiotasalokome@gmail.com>
2024-02-19 03:19:27 +0000

    initial commit

--- MinimalShoppingListApi/Program.cs
+++ MinimalShoppingListApi/Program.cs
@@ -1,44 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalShoppingListApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApiDbContext>(opt => opt.UseInMemoryDatabase("ShoppingListApi"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.MapGet("/shoppinglist", async(ApiDbContext db) => await db.Groceries.ToListAsync());
+
+app.MapGet("/shoppinglist/{id}", async (int id, ApiDbContext db) =>
+{
+    var grocery = await db.Groceries.FindAsync(id);
+
+    return grocery != null ? Results.Ok(grocery) : Results.NotFound();
+
+});
+
+app.MapPost("/shoppinglist", async (Grocery grocery, ApiDbContext db) =>
+{
+    db.Groceries.Add(grocery);
+
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/Shoppinglist/{grocery.Id}", grocery);
+});
+
+
+app.MapDelete("/shoppinglist/{id}", async (int id, ApiDbContext db) =>
+{
+
+    var grocery = await db.Groceries.FindAsync(id);
+
+    if (grocery != null)
+    {
+        db.Groceries.Remove(grocery);
+       await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
+if(app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
